f754ebf4507623c78110b21ddbf1e5536c3b55f2
Mehriniso <61561490+mehriniso27@users.noreply.github.com>
2022-02-02 20:53:26 -0500

    Create GreetingPanel.java

---
+++ GreetingPanel.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class GreetingsPanel extends JPanel{
+   JLabel greetingL;
+  
+   public GreetingsPanel(){
+       Font font = new Font("Serif", Font.BOLD + Font.ITALIC, 24);
+       greetingL = new JLabel("Welcome to Sunrise Specialty Catering");
+       greetingL.setFont(font);
+      
+       this.add(greetingL);
+   }
+}
